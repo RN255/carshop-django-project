0d32a21300cff3ce23ba4c81b8d619678e5899d8
Robert Nickson <106345947+RN255@users.noreply.github.com>
2023-04-18 13:39:31 +0100

    can upload one image in admin

--- carshop/models.py
+++ carshop/models.py
@@ -9,6 +9,8 @@ class Car(models.Model):
     car_build_year =  models.IntegerField()
     car_description =  models.TextField(null=True, blank=True)
     pub_date = models.DateTimeField("date published")
+    image_one = models.ImageField(upload_to='images', blank=True)
+
 
     def __str__(self):
         return self.car_manufacturer + " " + self.car_model + " " + str(self.car_build_year)
